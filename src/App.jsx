86185d9486c288bde03bf71e48717b24ad215335
67millwood <info@track2development.com>
2018-11-28 02:10:55 +0000

    added express server directory and connection code

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,9 @@ class App extends Component {
   }
 componentDidMount() {
   console.log("componentDidMount <App />");
+  const superSocket = new WebSocket("ws://localhost:3001/");
   setTimeout(() => {
-    console.log("Simulating incoming message");
+
     // Add a new message to the list of messages in the data store
     //const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
     //const messages = this.state.messages.concat(newMessage)
