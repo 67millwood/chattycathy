917da70e2550e2eaa0fa7b0b24995a191a50e3a7
67millwood <info@track2development.com>
2018-11-29 15:35:44 +0000

    different css for notifications as opposed to messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,15 +5,20 @@ import ReactDOM from 'react-dom';
 class Message extends Component {
   render () {
 
-
+    if (this.props.message.type === 'incomingNotification') {
+      return (
+        <div>
+            <span className="message-content">{this.props.message.content}</span>
+        </div>
+            )
+    } else {
     return (
       <div>
           <span className="message-username">{this.props.message.username}</span>
           <span className="message-content">{this.props.message.content}</span>
-          <span className="message-content">{this.props.message.type}</span>
       </div>);
 
           }
         }
-
+      }
 export default Message;
