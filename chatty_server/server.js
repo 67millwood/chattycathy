e01232804faff7f953061e9f77e72431788ef054
67millwood <info@track2development.com>
2018-11-29 16:43:24 +0000

    counter for users online working with conoslelog

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -39,7 +39,7 @@ createNotification = (notification) => {
 };
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  console.log(`Clients connected ${wss.clients.size}`);
   ws.on('message', function incoming(data) {
     const clean = JSON.parse(data)
     if (clean.type === 'postMessage') {
@@ -50,5 +50,6 @@ wss.on('connection', (ws) => {
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => console.log(`Clients connected ${wss.clients.size}`));
+  //
 });
